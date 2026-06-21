d0c92ecadc44bf5f43f0edda9dbdfed14540ccfb
Josehugo <josehugo.tec@gmail.com>
2026-06-21 04:25:33 -0500

    Update service worker to version 0.3.0 and improve caching

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,50 @@
-const CACHE_NAME = "socialgo-v0.1.0";
+const CACHE_NAME = "socialgo-v0.3.0";
+
 const CORE = [
   "./",
   "./index.html",
   "./login.html",
   "./registro.html",
+  "./recuperar.html",
+  "./restablecer.html",
+  "./auth-callback.html",
+  "./perfil.html",
   "./css/global.css",
   "./js/app.js",
+  "./js/auth.js",
+  "./js/brand.js",
   "./js/config.js",
-  "./js/brand.js"
+  "./js/env.public.js",
+  "./js/session.js",
+  "./js/supabase.js",
+  "./js/publicaciones.js"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE)));
-});
-
-self.addEventListener("activate", event => {
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-    ))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
   event.respondWith(
     fetch(event.request)
-      .then(response => {
+      .then((response) => {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
         return response;
       })
       .catch(() => caches.match(event.request))
